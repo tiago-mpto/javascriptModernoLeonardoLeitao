abc0a39efe0d2f8f31a64274ef13005148c765f5
Fabricio Lopes <fabiciolopes.unai@gmail.com>
2018-12-21 10:25:17 -0200

    3.27 - funcao2

---
+++ 3-fundamentos/3.27-funcao2.js
@@ -0,0 +1,17 @@
+//Armazenando um função em uma variavel
+const imprimirSoma = function (a, b) {
+    console.log(a+b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenando uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2,3))
+
+//retorno implícito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
